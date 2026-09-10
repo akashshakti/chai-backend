a5ad16df36b72d436d239f4617bf1b30bc5e0d09
akashshakti <av077075@gmail.com>
2026-09-10 10:09:07 +0530

    logic for user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,10 +1,24 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "ok" });
-    //const { username, email, password } = req.body;
-});
+    const { fullName, email, username, password } = req.body;
+    console.log('fullName:', fullName);
+    console.log('email:', email);
+    console.log('username:', username);
+    console.log('password:', password);
 
+
+    if (
+        [fullName, email, username, password].some((field) => 
+        field?.trim() === "")
+    ) {
+        throw new ApiError(400, "Please provide all required fields");
+    }
+
+    
+});
 // const loginUser = asyncHandler(async (req, res) => {
 //     res.status(200).json({ message: "ok" });
 //     //const { email, password } = req.body;
